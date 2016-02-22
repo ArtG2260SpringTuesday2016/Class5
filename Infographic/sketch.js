34be16071ef4d267e2ac681eba1fde92a196cd17
jillbook <jillbook@Jills-MacBook-Pro.local>
2016-02-21 23:23:26 -0500

    my infographic
    made some kind of infographic to show the number of pages per Harry
    Potter book by the size of circles

--- Infographic/sketch.js
+++ Infographic/sketch.js
@@ -1,18 +1,22 @@
-var baconWords = baconIpsum.split(' ');
-for (var i = 0; i < baconWords.length; i++) {
-  baconWords[i] = baconWords[i].length;
+//pages in each Harry Potter book
+var pagesData = [309,341,435,743,870,652,759];
+
+var ellipseNumbers = [1,2,3,4,5,6,7];
+var ellipseSide = 0;
+for (var j = 0; j<7; j++){
+  ellipseSide = ellipseNumbers[j]
 }
 
 function setup() {
   createCanvas(600,600)
 
-  for (var i = 0; i < baconWords.length; i++){
-    var rectHeight = baconWords[i] * 60;
-    var rectWidth = (width / baconWords.length);
+  for (var i = 0; i<pagesData.length; i++){
+    var ellipseHeight = pagesData[i]/10;
+    var ellipseWidth = pagesData[i]/10;
+    
 
-    fill('hotpink');
-    noStroke()
-    rect(i * rectWidth,width - rectHeight, rectWidth, rectHeight);
+    noFill()
+    ellipse(ellipseSide*j,(pagesData[i]/1.5)-150, ellipseWidth, ellipseHeight);
   }
 }
 
