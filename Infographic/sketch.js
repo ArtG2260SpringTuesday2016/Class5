2826147ca499bfe026bce16777688f0368d91d5a
AlexisDiefenbach <aodiefenbach@gmail.com>
2016-02-21 20:18:03 -0500

    graph
    based of shades of red and blue. i dont know how but i would also like
    to change each bar to the shade.

--- Infographic/sketch.js
+++ Infographic/sketch.js
@@ -1,18 +1,28 @@
-var baconWords = baconIpsum.split(' ');
-for (var i = 0; i < baconWords.length; i++) {
-  baconWords[i] = baconWords[i].length;
+var colorbl = colorb.split(' ');
+for (var i = 0; i < colorbl.length; i++) {
+  colorbl[i] = colorbl[i].length;
+}
+
+var colorre = colorr.split(' ');
+for (var i = 0; i < colorre.length; i++) {
+  colorre[i] = colorre[i].length;
 }
 
 function setup() {
-  createCanvas(600,600)
+  createCanvas(500,500)
 
-  for (var i = 0; i < baconWords.length; i++){
-    var rectHeight = baconWords[i] * 60;
-    var rectWidth = (width / baconWords.length);
+for (var i = 0; i < colorre.length; i++){
+ var rectHeight = colorre[i] * 50;
+ var rectWidth = (250 / colorre.length);
+ fill('red');
+ rect(i * rectWidth,width - rectHeight, rectWidth, rectHeight);
+  }
 
-    fill('hotpink');
-    noStroke()
-    rect(i * rectWidth,width - rectHeight, rectWidth, rectHeight);
+for (var i = 0; i < colorbl.length; i++){
+ var rectHeight = colorbl[i] * 50;
+ var rectWidth = (250 / colorbl.length);
+  fill('blue');
+  rect((i * rectWidth)+250,width - rectHeight, rectWidth, rectHeight);
   }
 }
 
