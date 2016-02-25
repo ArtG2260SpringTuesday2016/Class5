b4bf00ce41fd738566d56d9812d475b95f31ebe7
jillbook <jillbook@Jills-MacBook-Pro.local>
2016-02-24 23:05:51 -0500

    made changes
    made some suggested changes

--- Infographic/sketch.js
+++ Infographic/sketch.js
@@ -1,22 +1,21 @@
 //pages in each Harry Potter book
 var pagesData = [309,341,435,743,870,652,759];
 
-var ellipseNumbers = [1,2,3,4,5,6,7];
-var ellipseSide = 0;
-for (var j = 0; j<7; j++){
-  ellipseSide = ellipseNumbers[j]
-}
+
 
 function setup() {
   createCanvas(600,600)
 
+var ellipseSize = function(pagesData){
   for (var i = 0; i<pagesData.length; i++){
-    var ellipseHeight = pagesData[i]/10;
-    var ellipseWidth = pagesData[i]/10;
-    
+    var ellipseWidthAndHeight = pagesData[i]/10;
+  }
+return ellipseSize(pagesData);
+}
 
+  for (var i = 0; i<pagesData.length; i++){
     noFill()
-    ellipse(ellipseSide*j,(pagesData[i]/1.5)-150, ellipseWidth, ellipseHeight);
+    ellipse(300,(pagesData[i]/1.5)-150+ellipseSize, ellipseSize, ellipseSize);
   }
 }
 
