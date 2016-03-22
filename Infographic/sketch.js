5b52a3cc8f5f0d9d98935e611a060bd784505f4c
heyryanleys <ryanleys@gmail.com>
2016-03-22 12:45:44 -0400

    Moved work from private repo to correct location

--- Infographic/sketch.js
+++ Infographic/sketch.js
@@ -1,21 +1,29 @@
-var baconWords = baconIpsum.split(' ');
-for (var i = 0; i < baconWords.length; i++) {
-  baconWords[i] = baconWords[i].length;
+var swimmerNames = swimmers.split(' ');
+for (var i = 0; i < swimmerNames.length; i++) {
+  swimmerNames[i] = swimmerNames[i].length;
 }
 
 function setup() {
   createCanvas(600,600)
+  background(64,211,216)
 
-  for (var i = 0; i < baconWords.length; i++){
-    var rectHeight = baconWords[i] * 60;
-    var rectWidth = (width / baconWords.length);
+  for (var i = 0; i < swimmerNames.length; i++){
+    var rectHeight = swimmerNames[i] * 60;
+    var rectWidth = (width / swimmerNames.length * 2);
 
-    fill('hotpink');
+    fill('#FFFFFF');
     noStroke()
-    rect(i * rectWidth,width - rectHeight, rectWidth, rectHeight);
+    ellipse(i * 16, 300, rectWidth, rectHeight);
   }
+
 }
 
 function draw() {
-
+  textSize(32);
+  text("Swimmers Names By Length", 100, 50);
+  fill(0,0,100)
+  text("4 Letters", 10, 375);
+  text("5 Letters", 200, 300);
+  text("6 ", 375, 225);
+  text("7", 425, 150)
 }
