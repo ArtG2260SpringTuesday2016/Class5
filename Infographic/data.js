3f6fcba579c81019818a47c0a249d164a7e921df
Jacqueline-G <garruto.j@husky.neu.edu>
2016-03-28 14:10:16 -0400

    Moving location

---
+++ Infographic/data.js
@@ -0,0 +1,17 @@
+var typelist = 
+{  
+  "Akzidenz Grotesk" : 10,
+  "Avenir" : 9,
+  "Baskerville" : 8,
+  "Bembo" : 7,
+  "Helvetica" : 6,
+  "Bodoni"  : 5,
+  "Caslon" : 4,
+  "Clarendon" : 3,
+  "Dax" : 2,
+  "Frutiger" : 1
+};
+
+var xValue = 100;
+
+var yValue = 100;
--- Infographic/sketch.js
+++ Infographic/sketch.js
@@ -1,7 +1,47 @@
 function setup() {
+  createCanvas(1000, 1000);
+  drawCircles();
+  
+}
+function draw() {
   
 }
 
-function draw() {
+function drawCircles() {
+  for(var key in typelist) {
+    var fontName = key;
+    var rank = typelist[key];
+    console.log(fontName + '' + rank);
+    drawACircle(fontName, rank);
+  }
+}
+
+
+// helpler
+function drawACircle(fontName, rank) {
+  var size = 20;
+  var yFont = yValue;
+  var width = size * rank;
+  console.log(xValue);
   
+  if(xValue > (1000 - width)) {
+    yValue += 200;
+    yFont = yValue;
+    xValue = 100;
+  }
+  
+    if (width <= 100) {
+    xValue += 50;
+  }
+  
+  if(width <= 80) {
+    yFont += 60;
+  }
+  
+  console.log(width)
+  fill(color(255, 0, 0));
+  ellipse(xValue, yValue, width, width);
+  fill(color(0,0,0))
+  text(fontName, xValue-15, yFont);
+  xValue += (width + 10);
 }
